af2eb4c475473da0293bd1b77c2efcbebeeefd50
SushmitaSingh96 <sushmitasingh1996@gmail.com>
2022-12-01 17:47:10 +0100

    Commit the setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#from setuptools import setup
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name='Snow_Assignment_5',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    package = find_packages(),
+    author='sushmitasingh',
+    author_email='sushmitasingh1996@gmail.com',
+    install_requires=["numpy","turtles"],
+    description='setup.py for Snowflake '
+)
